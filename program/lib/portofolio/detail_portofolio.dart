c7da696e7d7468a949715857323b2c4f18d7d4db
indahrf <indahrestifauzi@upi.edu>
2023-05-23 12:45:57 +0700

    push portofolio page

---
+++ program/lib/portofolio/detail_portofolio.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const DetailPortofolioPage());
+}
+
+class DetailPortofolioPage extends StatelessWidget {
+  const DetailPortofolioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pendanaan',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/detail',
+      routes: {
+        // '/portofolio': (context) => const PortofolioPage(),
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pendanaan'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back), // Tambahkan ikon panah kembali
+            onPressed: () {
+              Navigator.of(context)
+                  .pop(); // Navigasikan kembali ke halaman sebelumnya
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 16.0),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.green,
+                            // Replace with your profile image
+                            // backgroundImage: AssetImage('your_image_path'),
+                          ),
+                          const SizedBox(width: 16),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Nama Pemilik UMKM',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'UMKM Name',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              Text(
+                                'Jenis Usaha',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              Text(
+                                '082xxxxxxxx',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'UMKM Details:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in pulvinar magna, at posuere felis. Nunc non finibus ante, vel blandit mauris.',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 8.0),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Detail Pendanaan',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in pulvinar magna, at posuere felis. Nunc non finibus ante, vel blandit mauris.',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: double.infinity,
+                  height: 120,
+                  color: Colors.grey[200],
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ProgressBar(
+                        value: 0.2,
+                        label: '20%',
+                        progressNumber: 'Progres 1',
+                        progressColor: Colors.green,
+                      ),
+                      ProgressBar(
+                        value: 0.4,
+                        label: '40%',
+                        progressNumber: 'Progres 2',
+                        progressColor: Colors.green,
+                      ),
+                      ProgressBar(
+                        value: 0.6,
+                        label: '60%',
+                        progressNumber: 'Progres 3',
+                        progressColor: Colors.green,
+                      ),
+                      ProgressBar(
+                        value: 0.8,
+                        label: '80%',
+                        progressNumber: 'Progres 4',
+                        progressColor: Colors.green,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 8.0),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Ulasan',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in pulvinar magna, at posuere felis. Nunc non finibus ante, vel blandit mauris.',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProgressBar extends StatelessWidget {
+  final double value;
+  final String label;
+  final String progressNumber;
+  final Color progressColor;
+  final Color backgroundColor;
+  final double strokeWidth;
+
+  const ProgressBar({
+    required this.value,
+    required this.label,
+    required this.progressNumber,
+    this.progressColor = Colors.green,
+    this.backgroundColor = Colors.grey,
+    this.strokeWidth = 4.0,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      height: 120,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          CircularProgressIndicator(
+            value: value,
+            backgroundColor: backgroundColor,
+            valueColor: AlwaysStoppedAnimation<Color>(progressColor),
+            strokeWidth: strokeWidth,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                progressNumber,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                label,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- program/lib/portofolio/portofolio_page.dart
+++ program/lib/portofolio/portofolio_page.dart
@@ -3,15 +3,11 @@ import '../marketplace/marketplace_page.dart';
 import '../profil/profil_page.dart';
 import '../beranda/main.dart';
 import '../assets/font.dart';
+import 'detail_portofolio.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
-}
-
-class PortofolioPage extends StatefulWidget {
-  @override
-  _PortofolioPageState createState() => _PortofolioPageState();
 }
 
 class MyApp extends StatelessWidget {
@@ -24,35 +20,55 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/', // Set the initial route to '/beranda'
+      initialRoute: '/', // Set the initial route to '/'
       routes: {
         '/': (context) => HomePage(),
         '/marketplace': (context) => MarketplacePage(),
         '/portofolio': (context) => PortofolioPage(),
+        // '/detail-portofolio': (context) => DetailPortofolioPage(),
         '/profil': (context) => ProfilPage(),
       },
     );
   }
 }
 
-class PortofolioStatePage extends StatefulWidget {
+class Portofolio {
+  String judul;
+  String deskripsi;
+  String tanggalAkhir;
+  bool selesai;
+
+  Portofolio({
+    required this.judul,
+    required this.deskripsi,
+    required this.tanggalAkhir,
+    required this.selesai,
+  });
+}
+
+class PortofolioPage extends StatefulWidget {
   @override
+  Portofolio? _selectedPortofolio;
   _PortofolioPageState createState() => _PortofolioPageState();
 }
 
 class _PortofolioPageState extends State<PortofolioPage> {
-  int _selectedIndex = 2; // Set default selected index to 1 (Portofolio)
+  int _selectedIndex = 2; // Set default selected index to 2 (Portofolio)
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    if (index == _selectedIndex) {
+      // Kembali ke halaman sebelumnya
+      Navigator.pop(context);
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
 
     // Navigate to the corresponding page based on the selected index
     switch (_selectedIndex) {
       case 0:
-        Navigator.pushReplacementNamed(context,
-            '/'); // Use pushReplacementNamed to replace the current page
+        Navigator.pushReplacementNamed(context, '/');
         break;
       case 1:
         Navigator.pushReplacementNamed(context, '/marketplace');
@@ -65,20 +81,69 @@ class _PortofolioPageState extends State<PortofolioPage> {
     }
   }
 
+  List<Portofolio> listPortofolio = [
+    Portofolio(
+      judul: 'Pendanaan A',
+      deskripsi: 'Deskripsi pendanaan A',
+      tanggalAkhir: '30 Mei 2023',
+      selesai: false,
+    ),
+    Portofolio(
+      judul: 'Pendanaan B',
+      deskripsi: 'Deskripsi pendanaan B',
+      tanggalAkhir: '15 Juni 2023',
+      selesai: true,
+    ),
+    Portofolio(
+      judul: 'Pendanaan C',
+      deskripsi: 'Deskripsi pendanaan C',
+      tanggalAkhir: '10 Juli 2023',
+      selesai: false,
+    ),
+  ];
+
+  void _navigateToPortofolioPage(BuildContext context, Portofolio portofolio) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailPortofolioPage(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Portofolio',
-          style: titleTextStyle,
-        ),
+        title: Text('Portofolio'),
       ),
-      body: Center(
-        child: Text(
-          'Portofolio',
-          style: titleTextStyle,
-        ),
+      body: ListView.builder(
+        itemCount: listPortofolio.length,
+        itemBuilder: (context, index) {
+          Portofolio portofolio = listPortofolio[index];
+          return Card(
+            child: ListTile(
+              title: Text(portofolio.judul),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(portofolio.tanggalAkhir),
+                  Text(portofolio.deskripsi),
+                ],
+              ),
+              trailing: Text(
+                portofolio.selesai ? 'Selesai' : 'Belum Selesai',
+                style: TextStyle(
+                  color: portofolio.selesai ? Colors.green : Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {
+                _navigateToPortofolioPage(context, portofolio);
+              },
+            ),
+          );
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -107,3 +172,49 @@ class _PortofolioPageState extends State<PortofolioPage> {
     );
   }
 }
+
+// class DetailPortofolioPage extends StatelessWidget {
+//   final Portofolio portofolio;
+
+//   DetailPortofolioPage({required this.portofolio});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Detail Portofolio'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Text(
+//               'Judul: ${portofolio.judul}',
+//               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(height: 16),
+//             Text(
+//               'Deskripsi: ${portofolio.deskripsi}',
+//               style: TextStyle(fontSize: 18),
+//             ),
+//             SizedBox(height: 16),
+//             Text(
+//               'Tanggal Akhir: ${portofolio.tanggalAkhir}',
+//               style: TextStyle(fontSize: 18),
+//             ),
+//             SizedBox(height: 16),
+//             Text(
+//               'Status: ${portofolio.selesai ? 'Selesai' : 'Belum Selesai'}',
+//               style: TextStyle(
+//                 fontSize: 18,
+//                 fontWeight: FontWeight.bold,
+//                 color: portofolio.selesai ? Colors.green : Colors.red,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
