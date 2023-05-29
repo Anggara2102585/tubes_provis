0133df6e03783247907d3942e27cfcfdfb58c17d
Anggara2102585 <anggarayudha585@upi.edu>
2023-05-29 23:00:57 +0700

    create pusatbantuan_page
    created two file, the "menujupusatbantuan.dart" is just a copy of "profil_page.dart" with added text button. the actual pusatbantuan page is in "pusatbantuan_page.dart"

---
+++ program/lib/profil/pusatbantuan_page.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import '../assets/font.dart';
+
+enum MetodePusatBantuan { transferBank, eWallet }
+
+class PusatBantuanPage extends StatefulWidget {
+  @override
+  _PusatBantuanPageState createState() => _PusatBantuanPageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Beranda',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PusatBantuanPage(),
+    );
+  }
+}
+
+class _PusatBantuanPageState extends State<PusatBantuanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'Pusat Bantuan',
+                style: titleTextStyle,
+              ),
+              backgroundColor: Colors.green,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              bottom: const TabBar(
+                tabs: [
+                  Tab(text: 'Customer Service'),
+                  Tab(text: 'Panduan & Tutorial'),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                CustomerService(),
+                PanduanTutorial(),
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomerService extends StatelessWidget {
+  final List<Map<String, dynamic>> contacts = [
+    {
+      'name': 'Instagram',
+      'account': '@example',
+      'icon': Icons.question_mark_rounded,
+    },
+    {
+      'name': 'Telepon',
+      'account': '+123456789',
+      'icon': Icons.phone,
+    },
+    {
+      'name': 'Email',
+      'account': 'example@example.com',
+      'icon': Icons.email,
+    },
+    // Add more contacts as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.separated(
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          final contact = contacts[index];
+          return ListTile(
+            leading: Icon(contact['icon']),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  contact['name'],
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Text(contact['account']),
+              ],
+            ),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              // Handle item click here
+              // Example: Open social media app or call app
+              if (contact['name'] == 'Instagram') {
+                // Open Instagram
+              } else if (contact['name'] == 'Telephone') {
+                // Make a phone call
+              } else if (contact['name'] == 'Email') {
+                // Compose an email
+              }
+            },
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+      ),
+    );
+  }
+}
+
+class PanduanTutorial extends StatelessWidget {
+  final List<Map<String, dynamic>> videos = [
+    {
+      'thumbnail': 'https://placehold.co/600x400/png',
+      'title': 'Video 1',
+    },
+    {
+      'thumbnail': 'https://placehold.co/600x400/png',
+      'title': 'Video 2',
+    },
+    // Add more videos as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          final video = videos[index];
+          return ListTile(
+            contentPadding: EdgeInsets.all(10),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    video['thumbnail'],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    video['title'],
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            onTap: () {
+              // Handle video item click here
+              // Example: Open video player screen
+            },
+          );
+        },
+      ),
+    );
+  }
+}
