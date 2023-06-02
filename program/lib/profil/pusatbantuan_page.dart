ced99f5f3f35e055f2d24b060b362959ceb5f234
Anggara2102585 <anggarayudha585@upi.edu>
2023-06-02 07:39:14 +0700

    add profil_page content
    add the main content for profil_page.dart and adjust pusatbantuan_page.dart a bit.

--- program/lib/profil/pusatbantuan_page.dart
+++ program/lib/profil/pusatbantuan_page.dart
@@ -4,8 +4,10 @@ import '../assets/font.dart';
 enum MetodePusatBantuan { transferBank, eWallet }
 
 class PusatBantuanPage extends StatefulWidget {
+  const PusatBantuanPage({Key? key}) : super(key: key);
+  
   @override
-  _PusatBantuanPageState createState() => _PusatBantuanPageState();
+  PusatBantuanPageState createState() => PusatBantuanPageState();
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -14,12 +16,12 @@ class PusatBantuanPage extends StatefulWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: PusatBantuanPage(),
+      home: const PusatBantuanPage(),
     );
   }
 }
 
-class _PusatBantuanPageState extends State<PusatBantuanPage> {
+class PusatBantuanPageState extends State<PusatBantuanPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,7 +38,7 @@ class _PusatBantuanPageState extends State<PusatBantuanPage> {
               ),
               backgroundColor: Colors.green,
               leading: IconButton(
-                icon: Icon(Icons.arrow_back),
+                icon: const Icon(Icons.arrow_back),
                 onPressed: () {
                   Navigator.pop(context);
                 },
@@ -50,8 +52,11 @@ class _PusatBantuanPageState extends State<PusatBantuanPage> {
             ),
             body: TabBarView(
               children: [
-                CustomerService(),
-                PanduanTutorial(),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: _CustomerService(),
+                ),
+                _PanduanTutorial(),
               ],
             )
           ),
@@ -61,7 +66,7 @@ class _PusatBantuanPageState extends State<PusatBantuanPage> {
   }
 }
 
-class CustomerService extends StatelessWidget {
+class _CustomerService extends StatelessWidget {
   final List<Map<String, dynamic>> contacts = [
     {
       'name': 'Instagram',
@@ -83,46 +88,44 @@ class CustomerService extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.separated(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          final contact = contacts[index];
-          return ListTile(
-            leading: Icon(contact['icon']),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  contact['name'],
-                  style: TextStyle(color: Colors.grey),
-                ),
-                Text(contact['account']),
-              ],
-            ),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () {
-              // Handle item click here
-              // Example: Open social media app or call app
-              if (contact['name'] == 'Instagram') {
-                // Open Instagram
-              } else if (contact['name'] == 'Telephone') {
-                // Make a phone call
-              } else if (contact['name'] == 'Email') {
-                // Compose an email
-              }
-            },
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider();
-        },
-      ),
+    return ListView.separated(
+      itemCount: contacts.length,
+      itemBuilder: (context, index) {
+        final contact = contacts[index];
+        return ListTile(
+          leading: Icon(contact['icon']),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                contact['name'],
+                style: const TextStyle(color: Colors.grey),
+              ),
+              Text(contact['account']),
+            ],
+          ),
+          trailing: const Icon(Icons.chevron_right),
+          onTap: () {
+            // Handle item click here
+            // Example: Open social media app or call app
+            if (contact['name'] == 'Instagram') {
+              // Open Instagram
+            } else if (contact['name'] == 'Telephone') {
+              // Make a phone call
+            } else if (contact['name'] == 'Email') {
+              // Compose an email
+            }
+          },
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
     );
   }
 }
 
-class PanduanTutorial extends StatelessWidget {
+class _PanduanTutorial extends StatelessWidget {
   final List<Map<String, dynamic>> videos = [
     {
       'thumbnail': 'https://placehold.co/600x400/png',
@@ -137,39 +140,42 @@ class PanduanTutorial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        itemCount: videos.length,
-        itemBuilder: (context, index) {
-          final video = videos[index];
-          return ListTile(
-            contentPadding: EdgeInsets.all(10),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.network(
-                    video['thumbnail'],
-                    fit: BoxFit.cover,
+    return ListView.builder(
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final video = videos[index];
+        return ListTile(
+          contentPadding: const EdgeInsets.all(16),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      video['thumbnail'],
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    video['title'],
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      video['title'],
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            onTap: () {
-              // Handle video item click here
-              // Example: Open video player screen
-            },
-          );
-        },
-      ),
+                ],
+              ),
+            ],
+          ),
+          onTap: () {
+            // Handle video item click here
+            // Example: Open video player screen
+          },
+        );
+      },
     );
   }
 }
