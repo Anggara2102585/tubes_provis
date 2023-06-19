ab64738c5a5a3e9efe876e290cd549060a7b5cb6
Khana Yusdiana <96195312+marimoo0@users.noreply.github.com>
2023-06-19 23:33:52 +0700

    updatemarketplace

--- program/lib/investor/marketplace/marketplace_page.dart
+++ program/lib/investor/marketplace/marketplace_page.dart
@@ -1,91 +1,31 @@
 import 'package:flutter/material.dart';
-import '../../shared_pref.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Marketplace App',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MarketplacePage(),
-    );
-  }
-}
-
-class MarketplacePage extends StatefulWidget {
-  @override
-  _MarketplacePageState createState() => _MarketplacePageState();
-}
-
-class _MarketplacePageState extends State<MarketplacePage> {
+class MarketplaceCubit extends ChangeNotifier {
   String searchQuery = '';
-  int _selectedIndex = 1;
-
-  //SharedPref
-  int id_akun = 0;
-  int jenis_user = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _initIdAkun();
-  }
-
-  Future<void> _initIdAkun() async {
-    MySharedPrefs sharedPrefs = MySharedPrefs();
-    await sharedPrefs.getId(context);
-    setState(() {
-      id_akun = sharedPrefs.id_akun;
-      jenis_user = sharedPrefs.jenis_user;
-    });
-  }
 
   void setSearchQuery(String query) {
-    setState(() {
-      searchQuery = query;
-    });
+    searchQuery = query;
+    notifyListeners();
   }
 
-  String formatCurrency(double amount) {
-    String formattedValue = 'Rp ' +
-        amount.toStringAsFixed(0).replaceAllMapped(
-              RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
-              (Match match) => '${match[1]}.',
-            );
-    return formattedValue;
-  }
+  void showFilterOptions() {}
+}
 
-  void _onItemTapped(int index) {
-    if (index == _selectedIndex) {
-      // Kembali ke halaman sebelumnya
-      Navigator.pop(context);
+class MarketplacePage extends StatelessWidget {
+  Future<List<dynamic>> fetchData() async {
+    final String apiUrl = 'http://127.0.0.1:8000/marketplace';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      return responseData['data'];
     } else {
-      setState(() {
-        _selectedIndex = index;
-      });
+      throw Exception('Failed to fetch data from the API');
     }
-
-    // Navigate to the corresponding page based on the selected index
-    switch (_selectedIndex) {
-      case 0:
-        Navigator.pushNamed(context, '/beranda');
-        break;
-      case 1:
-        // do nothing
-        break;
-      case 2:
-        Navigator.pushNamed(context, '/portofolio');
-        break;
-      case 3:
-        Navigator.pushNamed(context, '/profil');
-        break;
-    }
-  }
-
-  void showFilterOptions() {
-    // Implement filter options logic here
   }
 
   @override
@@ -97,112 +37,107 @@ class _MarketplacePageState extends State<MarketplacePage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        onChanged: setSearchQuery,
-                        decoration: InputDecoration(
-                          labelText: 'Cari Pendanaan',
-                          suffixIcon: Icon(Icons.search),
-                        ),
+          child: FutureBuilder<List<dynamic>>(
+            future: fetchData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final List<dynamic> data = snapshot.data!;
+
+                return Column(
+                  children: data.map((item) {
+                    final String namaUmkm = item['nama_umkm'];
+                    final int persenProgres = item['peersen_progres'];
+                    final double totalPendanaan = item['total_pendanaan'];
+                    final String dlPenggalanganDana =
+                        item['dl_penggalangan_dana'];
+                    final int kodePendanaan = item['kode_pendanaan'];
+                    final int imbaHasil = item['imba_hasil'];
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/detail-marketplace');
+                      },
+                      child: UMKMCard(
+                        nama_umkm: namaUmkm,
+                        peersen_progres: persenProgres,
+                        total_pendanaan: totalPendanaan,
+                        dl_penggalangan_dana: dlPenggalanganDana,
+                        kode_pendanaan: kodePendanaan,
+                        imba_hasil: imbaHasil,
                       ),
-                    ),
-                    SizedBox(width: 16.0),
-                    ElevatedButton.icon(
-                      onPressed: showFilterOptions,
-                      icon: Icon(Icons.filter_list),
-                      label: Text('Filter'),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20.0),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/detail-marketplace');
-                },
-                child: UMKMCard(
-                  name: 'UMKM ABC',
-                  progress: 70,
-                  totalLoan: 5000000,
-                  date: 'June 1, 2023',
-                  id: 123,
-                  bagiHasil: 8,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/detail-marketplace');
-                },
-                child: UMKMCard(
-                  name: 'UMKM XYZ',
-                  progress: 50,
-                  totalLoan: 3000000,
-                  date: 'June 2, 2023',
-                  id: 234,
-                  bagiHasil: 8,
-                ),
-              ),
-            ],
+                    );
+                  }).toList(),
+                );
+              } else if (snapshot.hasError) {
+                return Text('Failed to fetch data from the API');
+              }
+
+              return CircularProgressIndicator();
+            },
           ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Beranda',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart),
-              label: 'Marketplace',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.pie_chart),
-              label: 'Portofolio',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profil',
-            ),
-          ],
-          currentIndex:
-              _selectedIndex, // Set the current index to 1 (Marketplace)
-          selectedItemColor: Colors.green,
-          unselectedItemColor: Colors.green[100],
-          onTap: _onItemTapped),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Marketplace',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            label: 'Portofolio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: 1,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.green[100],
+        onTap: (index) {
+          if (index == 1) {
+            // Do nothing or handle marketplace page logic
+          } else {
+            switch (index) {
+              case 0:
+                Navigator.pushReplacementNamed(context, '/beranda');
+                break;
+              case 2:
+                Navigator.pushReplacementNamed(context, '/portofolio');
+                break;
+              case 3:
+                Navigator.pushReplacementNamed(context, '/profil');
+                break;
+            }
+          }
+        },
+      ),
     );
   }
 }
 
-class UMKMCard extends StatefulWidget {
-  final String name;
-  final int progress;
-  final double totalLoan;
-  final String date;
-  final int id;
-  final int bagiHasil;
+class UMKMCard extends StatelessWidget {
+  final String nama_umkm;
+  final int peersen_progres;
+  final double total_pendanaan;
+  final String dl_penggalangan_dana;
+  final int kode_pendanaan;
+  final int imba_hasil;
 
   UMKMCard({
-    required this.name,
-    required this.progress,
-    required this.totalLoan,
-    required this.date,
-    required this.id,
-    required this.bagiHasil,
+    required this.nama_umkm,
+    required this.peersen_progres,
+    required this.total_pendanaan,
+    required this.dl_penggalangan_dana,
+    required this.kode_pendanaan,
+    required this.imba_hasil,
   });
 
-  @override
-  _UMKMCardState createState() => _UMKMCardState();
-}
-
-class _UMKMCardState extends State<UMKMCard> {
   String formatCurrency(double amount) {
     String formattedValue = 'Rp ' +
         amount.toStringAsFixed(0).replaceAllMapped(
@@ -228,7 +163,7 @@ class _UMKMCardState extends State<UMKMCard> {
                   child: Row(
                     children: <Widget>[
                       Text(
-                        widget.name,
+                        nama_umkm,
                         style: TextStyle(
                           fontSize: 20.0,
                           fontWeight: FontWeight.bold,
@@ -237,13 +172,15 @@ class _UMKMCardState extends State<UMKMCard> {
                       SizedBox(width: 10.0),
                       Container(
                         padding: EdgeInsets.symmetric(
-                            horizontal: 10.0, vertical: 5.0),
+                          horizontal: 10.0,
+                          vertical: 5.0,
+                        ),
                         decoration: BoxDecoration(
                           color: Colors.green,
                           borderRadius: BorderRadius.circular(5.0),
                         ),
                         child: Text(
-                          'Id: ${widget.id}',
+                          'Id: $kode_pendanaan',
                           style: TextStyle(
                             fontSize: 14.0,
                             color: Colors.white,
@@ -255,8 +192,10 @@ class _UMKMCardState extends State<UMKMCard> {
                   ),
                 ),
                 Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 5.0,
+                  ),
                   decoration: BoxDecoration(
                     color: Colors.green,
                     borderRadius: BorderRadius.circular(5.0),
@@ -275,7 +214,7 @@ class _UMKMCardState extends State<UMKMCard> {
                         ),
                         SizedBox(height: 4.0),
                         Text(
-                          '${widget.bagiHasil}%',
+                          '$imba_hasil%',
                           style: TextStyle(
                             fontSize: 14.0,
                             color: Colors.white,
@@ -306,44 +245,35 @@ class _UMKMCardState extends State<UMKMCard> {
                           ),
                         ),
                         SizedBox(width: 10.0),
-                        Container(
-                          height: 40.0,
-                          width: 40.0,
-                          child: Stack(
-                            children: [
-                              CircularProgressIndicator(
-                                value: widget.progress / 100,
-                                strokeWidth: 4.0,
-                                valueColor:
-                                    AlwaysStoppedAnimation<Color>(Colors.green),
-                                backgroundColor: Colors.grey[300],
-                              ),
-                              Center(
-                                child: Text(
-                                  '${widget.progress}%',
-                                  style: TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ],
+                        Text(
+                          '$peersen_progres%',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ],
                     ),
                     SizedBox(height: 8.0),
                     Text(
-                      'Total Pinjaman: ${formatCurrency(widget.totalLoan)}',
-                      style: TextStyle(fontSize: 16.0),
+                      'Jumlah Pinjaman: ${formatCurrency(total_pendanaan)}',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     SizedBox(height: 8.0),
                     Text(
-                      'Tanggal crowdfunding: ${widget.date}',
-                      style: TextStyle(fontSize: 16.0),
+                      'Tanggal: $dl_penggalangan_dana',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
+                SizedBox(width: 16.0),
               ],
             ),
           ],
